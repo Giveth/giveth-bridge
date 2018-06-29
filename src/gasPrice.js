875223ae399a2432276edb9ba05a914c735febae
perissology <perissology@protonmail.com>
2018-06-29 14:57:06 -0700

    pass number as string to toWei func

--- src/gasPrice.js
+++ src/gasPrice.js
@@ -7,29 +7,29 @@ let lastChecked = Date.now() - FIVE_MINUTES - 1;
 let lastPrice = 1000000000;
 
 const queryGasStation = () => {
-  if (Date.now() > lastChecked + FIVE_MINUTES) {
-    return rp('https://ethgasstation.info/json/ethgasAPI.json')
-      .then(resp => {
-        const { average } = JSON.parse(resp);
-        lastPrice = utils.toWei(average / 10, 'gwei'); // response in gwei * 10
-        lastChecked = Date.now();
-        return lastPrice;
-      })
-      .catch(e => {
-        console.error('could not fetch gas from ethgasstation')
-        return lastPrice;
-      });
-  }
+    if (Date.now() > lastChecked + FIVE_MINUTES) {
+        return rp('https://ethgasstation.info/json/ethgasAPI.json')
+            .then(resp => {
+                const { average } = JSON.parse(resp);
+                lastPrice = utils.toWei(`${average / 10}`, 'gwei'); // response in gwei * 10
+                lastChecked = Date.now();
+                return lastPrice;
+            })
+            .catch(e => {
+                console.error('could not fetch gas from ethgasstation', e);
+                return lastPrice;
+            });
+    }
 
-  return Promise.resolve(lastPrice);
-}
+    return Promise.resolve(lastPrice);
+};
 
 export default (config, homeNetwork = true) => {
-  const gasPrice = homeNetwork ? config.homeGasPrice : config.foreignGasPrice;
+    const gasPrice = homeNetwork ? config.homeGasPrice : config.foreignGasPrice;
 
-  if (gasPrice === 'ethGasStation') {
-    return queryGasStation();
-  }
+    if (gasPrice === 'ethGasStation') {
+        return queryGasStation();
+    }
 
-  return Promise.resolve(gasPrice);
+    return Promise.resolve(gasPrice);
 };
