e8124946c1d3d8b22b9d88f7e830ebba06679a75
Amin Latifi <a.latifi.al@gmail.com>
2021-05-05 17:16:55 +0430

    Fix some lintig issues

--- src/Relayer.js
+++ src/Relayer.js
@@ -1,6 +1,6 @@
-import Web3 from 'web3';
+/* eslint-disable consistent-return */
+// eslint-disable-next-line max-classes-per-file
 import logger from 'winston';
-import uuidv4 from 'uuid/v4';
 import GivethBridge from './GivethBridge';
 import ForeignGivethBridge from './ForeignGivethBridge';
 import getGasPrice from './gasPrice';
@@ -31,6 +31,7 @@ export default class Relayer {
     constructor(homeWeb3, foreignWeb3, nonceTracker, config, db) {
         this.homeWeb3 = homeWeb3;
         this.foreignWeb3 = foreignWeb3;
+        // eslint-disable-next-line prefer-destructuring
         this.account = homeWeb3.eth.accounts.wallet[0];
         this.nonceTracker = nonceTracker;
 
@@ -45,8 +46,8 @@ export default class Relayer {
 
         this.db = db;
         this.config = config;
-        this.pollingPromise;
-        this.bridgeData;
+        // this.pollingPromise;
+        // this.bridgeData;
     }
 
     /* istanbul ignore next */
@@ -57,7 +58,7 @@ export default class Relayer {
             // so do it now
             this.relayUnsentTxs();
 
-            const intervalId = setInterval(() => {
+            setInterval(() => {
                 if (this.pollingPromise) {
                     logger.debug('Already polling, running after previous round finishes');
                     this.pollingPromise.finally(() => {
@@ -77,12 +78,11 @@ export default class Relayer {
         const { sender, mainToken, amount, data, homeTx } = tx;
 
         if (!tx.sideToken) {
-            this.updateTxData(
-                Object.assign({}, tx, {
-                    status: 'failed-send',
-                    error: 'No sideToken for mainToken',
-                }),
-            );
+            this.updateTxData({
+                ...tx,
+                status: 'failed-send',
+                error: 'No sideToken for mainToken',
+            });
             return Promise.resolve();
         }
 
@@ -102,7 +102,7 @@ export default class Relayer {
                     .on('transactionHash', transactionHash => {
                         txHash = transactionHash;
                         this.nonceTracker.releaseNonce(nonce);
-                        this.updateTxData(Object.assign({}, tx, { txHash, status: 'pending' }));
+                        this.updateTxData({ ...tx, txHash, status: 'pending' });
                     });
             })
             .catch((error, receipt) => {
@@ -111,12 +111,7 @@ export default class Relayer {
                 // if we have a txHash, then we will pick up the failure in the Verifyer
                 if (!txHash) {
                     this.nonceTracker.releaseNonce(nonce, false, false);
-                    this.updateTxData(
-                        Object.assign({}, tx, {
-                            error,
-                            status: 'failed-send',
-                        }),
-                    );
+                    this.updateTxData({ ...tx, error, status: 'failed-send' });
                 }
             });
     }
@@ -153,12 +148,7 @@ export default class Relayer {
                 // if we have a homeTxHash, then we will pick up the failure in the Verifyer
                 if (!txHash) {
                     this.nonceTracker.releaseNonce(nonce, true, false);
-                    this.updateTxData(
-                        Object.assign({}, tx, {
-                            status: 'failed-send',
-                            error,
-                        }),
-                    );
+                    this.updateTxData({ ...tx, status: 'failed-send', error });
                 }
             });
     }
@@ -234,13 +224,15 @@ export default class Relayer {
                 // catch error fetching block or gasPrice
                 logger.error('Error occured fetching blockNumbers or gasPrice ->', err);
             })
-            .finally(() => (this.pollingPromise = undefined));
+            .finally(() => {
+                this.pollingPromise = undefined;
+            });
 
         return this.pollingPromise;
     }
 
     loadBridgeData() {
-        const bridgeData = Object.assign({}, BridgeData);
+        const bridgeData = { ...BridgeData };
 
         return new Promise((resolve, reject) => {
             this.db.bridge.findOne({}, (err, doc) => {
@@ -277,11 +269,10 @@ export default class Relayer {
                                 resolve();
                             }
 
-                            const promises = docs.map(
-                                tx =>
-                                    tx.toHomeBridge
-                                        ? this.sendHomeTx(tx, homeGP)
-                                        : this.sendForeignTx(tx, foreignGP),
+                            const promises = docs.map(tx =>
+                                tx.toHomeBridge
+                                    ? this.sendHomeTx(tx, homeGP)
+                                    : this.sendForeignTx(tx, foreignGP),
                             );
                             Promise.all([...promises]).then(() => resolve());
                         });
@@ -325,10 +316,10 @@ export default class Relayer {
                     return;
                 }
 
-                this.db.txs.insert(tx, (err, doc) => {
-                    if (err) {
-                        logger.error('Error inserting bridge-txs.db ->', err, data);
-                        reject(error);
+                this.db.txs.insert(tx, (e, doc) => {
+                    if (e) {
+                        logger.error('Error inserting bridge-txs.db ->', e, data);
+                        reject(e);
                     }
                     resolve(doc);
                 });
