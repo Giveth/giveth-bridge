3636904ed5d3d2e22d3ccef2e5e737fb4a388443
Amin Latifi <a.latifi.al@gmail.com>
2021-05-05 19:27:26 +0430

    Put bridge address in balance alert mail

--- src/checkBalance.js
+++ src/checkBalance.js
@@ -10,7 +10,7 @@ const checkBalance = (config, web3) => {
         const balanceLimit = Number(config.balanceLimit || 0.1);
 
         if (balanceEther < balanceLimit) {
-            const msg = `Bridge balance is less than limit\n\n    balance: ${balanceEther}`;
+            const msg = `Bridge balance is less than limit\n\n    bridge address: ${address}\n\n    balance: ${balanceEther}`;
             logger.error(msg);
             sendEmail(config, msg);
         }
