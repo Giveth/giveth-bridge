232c563e7113ef70b2292faa5dc9a708093765d8
Amin Latifi <a.latifi.al@gmail.com>
2021-05-05 17:58:27 +0430

    Fix bug in computing limit

--- src/checkBalance.js
+++ src/checkBalance.js
@@ -5,12 +5,12 @@ const checkBalance = (config, web3) => {
     const { address } = web3.eth.accounts.wallet[0];
 
     web3.eth.getBalance(address).then(balanceWei => {
-        const { fromWei, toBN } = web3.utils;
-        const balanceEther = toBN(fromWei(balanceWei));
-        const balanceLimit = toBN(config.balanceLimit || 0.1);
+        const { fromWei } = web3.utils;
+        const balanceEther = Number(fromWei(balanceWei));
+        const balanceLimit = Number(config.balanceLimit || 0.1);
 
-        if (balanceEther.lt(balanceLimit)) {
-            const msg = `Bridge balance is less than limit\n\n    balance: ${balanceEther.toString()}`;
+        if (balanceEther < balanceLimit) {
+            const msg = `Bridge balance is less than limit\n\n    balance: ${balanceEther}`;
             logger.error(msg);
             sendEmail(config, msg);
         }
